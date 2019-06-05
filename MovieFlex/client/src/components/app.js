2ebe795844c5c2b066b08e39edbaeffa19e370f8
KeumsungHwang <keumsung.eric@gmail.com>
2019-06-05 16:57:50 -0400

    updated master.css

--- MovieFlex/client/src/components/app.js
+++ MovieFlex/client/src/components/app.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Header from './base/header';
-import Footer from './base/Footer';
+import React from "react";
+import Header from "./base/Header";
+import Footer from "./base/Footer";
 import Movies from "./Movies";
-// import MainCarousel from "./MainCarousel";
-export default class App extends React.Component{
-    render(){
-        return(
-        <div>
-            <Header/>
-            {/* <MainCarousel /> */}
-            <Movies />
-            <Footer/>
-        </div>
-        )
 
-    }
+// import MainCarousel from "./MainCarousel";
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        {/* <MainCarousel /> */}
+        <Movies />
+        <Footer />
+      </div>
+    );
+  }
 }
